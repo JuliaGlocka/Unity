732f1a5e88b455d59313ae77bad3ad7a49d6ad8f
Julia Glocka <glockajulia@gmail.com>
2023-06-09 21:48:50 +0200

    Some of the project scripts made by me

---
+++ UpDown.cs
@@ -0,0 +1,49 @@
+using UnityEngine;
+
+public class UpDown : MonoBehaviour
+{
+    [SerializeField] private Transform targetA, targetB;
+    [SerializeField] private float speed = 4f; // Change this to suit your game.
+    private bool movingToB = true;
+
+    private Vector3 _direction;
+
+    public Vector3 Direction { get => _direction; set => _direction = value; }
+    public float Speed { get => speed; set => speed = value; }
+
+    void FixedUpdate()
+    {
+        if (movingToB)
+        {
+            MoveToTarget(targetB);
+        }
+        else
+        {
+            MoveToTarget(targetA);
+        }
+    }
+
+    private void MoveToTarget(Transform target)
+    {
+        transform.position = Vector3.MoveTowards(transform.position, target.position, speed * Time.deltaTime);
+
+        if (transform.position == target.position)
+        {
+            movingToB = !movingToB;
+            return;
+        }
+        _direction = (target.position - transform.position).normalized;
+    }
+    //Player stays on the platform, rotates with the platform
+    //private void OnTriggerEnter(Collider other)
+    //{
+    //    other.transform.SetParent(transform);
+    //}
+
+    //private void OnTriggerExit(Collider other)
+    //{
+    //    other.transform.SetParent(null);
+    //}
+}
+
+
